93226fb693dbc9c37b39cec8f2019ddc22230908
matixbingo <matixbingo@gmail.com>
2017-05-31 18:01:50 +0800

    add

---
+++ src/base/Action.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * 组件的双向绑定用，修改对应path的数据，继承可选
+ * 例：a.b.c修改model a的b.c值
+ */
+export default class Action {
+    static setValueByReducers(valueLink, val){
+        (!valueLink || !val) && window.console.error('Action valueLink or val is undifened', valueLink, val)
+        valueLink = valueLink.match(/\.(.+?)$/, valueLink)[1]
+        return this.update(valueLink, val)
+    }
+}

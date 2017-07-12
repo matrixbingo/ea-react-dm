7df9c81067b161b3f2d95ecc596475794a5064af
matixbingo <matixbingo@gmail.com>
2017-07-12 14:04:27 +0800

    add

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export {Model} from './model'
 export {Control,Sync,fetch} from './control'
 export {View} from './view'
 export Action from './base/Action'
+export BaseControl from './base/BaseControl'
 //export {binding} from './valuelink'
 
 import extend from 'extend'
